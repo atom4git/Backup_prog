e42b3e09bb07b104c541233788993109dcf09a5c
Sergey <atom.4mail@gmail.com>
2019-09-22 17:12:55 +0300

    version 0.0.3 change archiving method minor improvements
    Signed-off-by: Sergey <atom.4mail@gmail.com>

--- System_pass.py
+++ System_pass.py
@@ -2,13 +2,17 @@
 # Архивирует файлы и складывает их по дате
 # данные берет из path_file.py файла с данными
 
-import datetime, os, ftplib  # import modules shutil
-import path_list
+import datetime, os, ftplib, zipfile  # import modules
+import path_list  # list with settings
 
+# import settings
 path_to_file = path_list.path_to_file  # path to backup
 arch_obj = path_list.arch_obj  # folder or file to backup
 debug = path_list.debug  # debug level
 backup_objects = path_list.path_to_backup
+path_for_backup = path_list.path_for_backup
+
+home_dir = os.getcwd()  # get script home dir
 
 
 def notest_file(text):
@@ -28,61 +32,72 @@ def get_date(format_of_date):
     return current_date
 
 
-def zip_file(arch_obj):
+def zip_file(backup_objects):
     """create zip file end incert information in it"""
 
     # 1. Файлы и каталоги, которые необходимо скопировать, собираются в список.
-    source = backup_objects
+    # source = backup_objects
     # Заметьте, что для имён, содержащих пробелы, необходимо использовать
     # двойные кавычки внутри строки.
     # 2. Резервные копии должны храниться в основном каталоге резерва.
-    if not os.path.exists(path_to_file + "\\backup"):
-        os.makedirs(path_to_file + "\\backup")
-    target_dir = "d:\\programm\\python\\projects\\backup_prog\\backup\\backup"  # Подставьте тот путь, который вы будете использовать.
+    if not os.path.exists(path_for_backup):
+        os.makedirs(path_for_backup)
+    # target_dir = "d:\\programm\\python\\projects\\backup_prog\\backup\\backup"  # Подставьте тот путь, который вы будете использовать.
     # 3. Файлы помещаются в zip-архив.
     # 4. Именем для zip-архива служит текущая дата и время.
     name_of_zip_file = (get_date("%d%m%Y_%H.%S") + '.zip')
-    target = target_dir + os.sep + name_of_zip_file
+    # target = target_dir + os.sep + name_of_zip_file
     # print(target)
-    # 5. Используем команду "zip" для помещения файлов в zip-архив
-    zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
+    # помещения файлов в zip-архив
+    z = zipfile.ZipFile(name_of_zip_file, 'a')  # Создание нового архива
+    for i in backup_objects:
+        if os.path.isdir(i):
+            for root, dirs, files in os.walk(i):  # Список всех файлов и папок в директории folder
+                for file in files:
+                    z.write(os.path.join(root, file))  # Создание относительных путей и запись файлов в архив
+        else:
+            z.write(i)
+    z.close()
+    if zipfile.is_zipfile(name_of_zip_file):
+        notest_file("arckhiving is conplite!")
     # Запускаем создание резервной копии
-    if os.system(zip_command) == 0:
-        notest_file('Suchesful' + target)
-    else:
-        notest_file('Somthing wrong with arhiving')
+    # if os.system(zip_command) == 0:
+    #     notest_file('Suchesful' + target)
+    # else:
+    #     notest_file('Somthing wrong with arhiving')
     return name_of_zip_file
 
 
-def test_path():
-    """
-    verify file or directory availability. If file or directory does not exist:
-    :return: close program
-    """
-    testpath = path_to_file + arch_obj
-    notest_file(testpath)  # action log
-
-    if os.path.exists(testpath):
-        if os.path.isfile(testpath):
-            notest_file('File')  # action log
-            notest_file('Size:' + str(os.path.getsize(testpath) // 1024) + 'Kb')  # action log
-            notest_file(
-                'Criate date:' + str(datetime.datetime.fromtimestamp(int(os.path.getctime(testpath)))))  # action log
-            notest_file(
-                'Date last open:' + str(datetime.datetime.fromtimestamp(int(os.path.getatime(testpath)))))  # action log
-            notest_file(
-                'Date last change:' + str(
-                    datetime.datetime.fromtimestamp(int(os.path.getmtime(testpath)))))  # action log
-        elif os.path.isdir(testpath):
-            notest_file('Direcrory')  # action log
-            notest_file('Files in Dir: ' + str(os.listdir(testpath)))  # action log
-    else:
-        notest_file("object not found")  # action log
-        os.abort()
+# def test_path():
+#     """
+#     verify file or directory availability. If file or directory does not exist:
+#     :return: close program
+#     """
+#     testpath = (i, for i in backup_objects)
+#     notest_file(testpath)  # action log
+#
+#     if os.path.exists(testpath):
+#         if os.path.isfile(testpath):
+#             notest_file('File')  # action log
+#             notest_file('Size:' + str(os.path.getsize(testpath) // 1024) + 'Kb')  # action log
+#             notest_file(
+#                 'Criate date:' + str(datetime.datetime.fromtimestamp(int(os.path.getctime(testpath)))))  # action log
+#             notest_file(
+#                 'Date last open:' + str(datetime.datetime.fromtimestamp(int(os.path.getatime(testpath)))))  # action log
+#             notest_file(
+#                 'Date last change:' + str(
+#                     datetime.datetime.fromtimestamp(int(os.path.getmtime(testpath)))))  # action log
+#         elif os.path.isdir(testpath):
+#             notest_file('Direcrory')  # action log
+#             notest_file('Files in Dir: ' + str(os.listdir(testpath)))  # action log
+#     else:
+#         notest_file("object not found")  # action log
+#         os.abort()
 
 
 def copy_to_ftp(path):
     # login data
+    os.chdir(home_dir)
     host = path_list.ip_server  # get server's IP from config file
     ftp_user = path_list.server_log  # get user's Login from config file
     ftp_password = path_list.server_pass  # get user's Password from config file
@@ -94,7 +109,7 @@ def copy_to_ftp(path):
 
     # list_dir = os.listdir()
     # print(ftp.nlst())
-    os.chdir(path_to_backup)
+    os.chdir(path_for_backup)
     # print(os.getcwd())
     list_dir = os.listdir()
 
@@ -105,15 +120,19 @@ def copy_to_ftp(path):
     ftp.quit()
 
 
-os.chdir(path_to_file)
+# os.chdir(path_to_file)
 notest_file("start: " + get_date("%d%m%Y_%H:%S"))
 
-test_path()
-os.chdir(path_to_file)
+# test_path()
+# os.chdir(path_to_file)
 
-name_of_zip_file = zip_file(arch_obj)
-notest_file(name_of_zip_file)  # action log
-path_to_backup = path_to_file + "\\backup\\"
-copy_to_ftp(path_to_backup)
-os.chdir(path_to_file)
+name_of_zip_file = zip_file(backup_objects)
+# notest_file(name_of_zip_file)  # action log
+# path_to_backup = path_to_file + "\\backup\\"
+# copy_to_ftp(name_of_zip_file)
+# os.chdir(path_to_file)
+os.rename((os.getcwd() + "\\" + name_of_zip_file), (path_for_backup + "\\" + name_of_zip_file))
+# os.chdir(path_for_backup)
+copy_to_ftp(name_of_zip_file)
+os.chdir(home_dir)
 notest_file("end: " + get_date("%d%m%Y_%H:%S") + "\n" + "=" * 10 + "\n")  # action log
--- path_list.py
+++ path_list.py
@@ -1,6 +1,12 @@
 path_to_file = "d:\\programm\\python\\projects\\backup_prog\\backup\\"
 path_to_backup = ["d:\\programm\\python\\projects\\backup_prog\\backup\\folder",
-                  "d:\\programm\\python\\projects\\backup_prog\\backup\\folder1"]
+                  "d:\\programm\\python\\projects\\backup_prog\\backup\\folder1",
+                  "d:\\programm\\python\\projects\\backup_prog\\backup\\test.txt"]
+
+
+path_for_backup = "d:\\programm\\python\\projects\\backup_prog\\backup\\backup\\"
+
+
 arch_obj = "folder"
 debug = 2
 ip_server = "192.168.2.55"
